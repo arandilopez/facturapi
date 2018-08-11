6d3b862a4c02d41e53e5cc8f88a730de8323c891
Arandi Lopez <arandilopez.93@gmail.com>
2018-08-10 23:39:45 -0500

    Setup test key

--- test/facturapi_test.rb
+++ test/facturapi_test.rb
@@ -2,10 +2,10 @@ require "test_helper"
 
 class FacturapiTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Facturapi::VERSION
+    refute_nil Facturapi::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_that_it_can_config
+    assert Facturapi.respond_to?(:configure)
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,7 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "facturapi"
 
 require "minitest/autorun"
+
+Facturapi.configure do |config|
+  config.api_key = 'sk_test_r2QW0xXyOwNgnwqEPB3nKk8EJV6Yq43B'
+end
