fbbc83f82148973336f573de3c3d55f955d53870
Arandi Lopez <arandilopez.93@gmail.com>
2018-08-12 13:17:17 -0500

    Todo: test product and customer resources

---
+++ test/resources/customer_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+require "support/mock_responses"
+
+class CustomerTest < Minitest::Test
+  include Facturapi::MockResponses
+
+  def test_retreive_all_customers
+    #code
+  end
+
+  def test_retreive_customer_by_id
+    #code
+  end
+
+  def test_create_customer
+    #code
+  end
+
+  def test_update_customer
+    #code
+  end
+
+  def test_update_customer_via_instance
+    #code
+  end
+
+  def test_delete_customer
+    #code
+  end
+
+  def test_delete_customer_via_instance
+    #code
+  end
+end
---
+++ test/resources/product_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+require "support/mock_responses"
+
+class ProductTest < Minitest::Test
+  include Facturapi::MockResponses
+
+  def test_retreive_all_products
+    #code
+  end
+
+  def test_retreive_product_by_id
+    #code
+  end
+
+  def test_create_product
+    #code
+  end
+
+  def test_update_product
+    #code
+  end
+
+  def test_update_product_via_instance
+    #code
+  end
+
+  def test_delete_product
+    #code
+  end
+
+  def test_delete_product_via_instance
+    #code
+  end
+end
